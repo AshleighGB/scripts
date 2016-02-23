9d2d0c73c3f2715c2ee1caa8a02417e485c1ec40
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-02-23 18:14:01 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,5 +22,6 @@ wget https://raw.githubusercontent.com/JoshWareing/linux-setup-scripts/master/00
 wget https://raw.githubusercontent.com/JoshWareing/linux-setup-scripts/master/10-sysinfo
 wget https://raw.githubusercontent.com/JoshWareing/linux-setup-scripts/master/90-footer
 chmod +x *
+echo "******** this script is done. you must now logout and back in again to see the changes. ********"
 
 
