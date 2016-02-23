adfaa61b3ed85cad7d074a0cba36a13b4a5ac306
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-02-23 18:07:56 +0000

    Update and rename apache-mysql-php5-phpmyadmin.sh to config.sh

--- apache-mysql-php5-phpmyadmin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get -y update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-do-release-upgrade
-rm -rf /tmp/*
-apt-get -y install apache2 php5 php5 mysql-server mysql-client phpmyadmin shellinabox htop nload nmap
-apt-get -y autoclean
-apt-get -y autoremove
