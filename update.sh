685cde15433c0e596cd61a47d3bd4422e9f589a4
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-03-05 02:09:20 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get -y autoclean
-rm -rf update.sh
-rm -rf update.sh*
+sudo rm -rf update.sh
+sudo rm -rf update.sh*
