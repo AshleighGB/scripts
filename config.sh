4e3224e2db2333e768a38d734b30bfbcb4476877
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-03-06 00:50:10 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -18,14 +18,16 @@ sleep 2
 clear
 echo "******** adding standard user ********"
 adduser --quiet --disabled-password -shell /bin/bash --home /home/joshua --gecos "Joshua Wareing" joshua
+echo "******** user added successfully ********"
 sleep 2
 clear
 echo "joshua:wareing" | chpasswd
 echo "******** adding standard user to sudo group ********"
-adduser joshua sudo
+adduser joshua sudo --quiet
+echo "******** user added to 'sudo' group successfully ********"
 sleep 2
 clear
-echo "******** updating APT repositories ********"
+echo "******** updating APT repositories & removing any old packages ********"
 apt-get -y update && apt-get -y autoremove
 sleep 2
 clear
@@ -36,9 +38,18 @@ clear
 echo "******** restarting SSH Server (your connection may terminate(this CANNOT be helped.)) ********"
 service ssh restart
 sleep 3
+echo "******** SSH Server Restarted ********"
+sleep 1
 clear
 echo "******** ssh security has been successfully configured.  ********"
-echo "******** Running Installer (htop ) ********"
+sleep 3
+clear
+echo "******** Running Installer ( htop ) ********"
 apt-get -y install htop
+echo "******** Installer Completed ( htop ) ********"
+sleep 3
+echo "******** Running Installer ( shellinabox ) ********"
+apt-get -y install shellinabox
+echo "******** Installer Completed ( shellinabox ) ********"
 sleep 3
 clear
