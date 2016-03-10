0e3ff46137ca2a2274290e411645379a820006e5
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-03-10 18:30:10 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 clear
-echo "******** changing root password ********"
+echo "/ changing root password"
 echo "root:R0sebr1dge" | chpasswd
 sleep 2
 clear
-echo "******** making the login prompt cleaner ********"
+echo "+ applying custom configs for remote logins"
 touch /etc/skel/.hushlogin
 rm -rf /etc/motd
 rm -rf /etc/issue
@@ -12,32 +12,39 @@ rm -rf /root/.ssh/known_hosts
 rm -rf /root/.ssh/authorized_keys
 sleep 2
 clear
-echo "******** adding custom sshd_config ********"
-cd /etc/ssh/ && rm sshd_config && wget https://www.dropbox.com/s/05f96jvwddl09qo/sshd_config
+echo "- removing old sshd_config"
+cd /etc/ssh/ && rm sshd_config
+echo "+ adding custom sshd_config"
+wget https://www.dropbox.com/s/05f96jvwddl09qo/sshd_config
 sleep 2
 clear
-echo "******** adding standard user ********"
-adduser --quiet --disabled-password -shell /bin/bash --home /home/joshua --gecos "Joshua Wareing" joshua
-echo "******** user added successfully ********"
+echo "+ adding standard user"
+adduser --disabled-password -shell /bin/bash --home /home/joshua --gecos "Joshua Wareing" joshua
+echo "+ user added successfully "
 sleep 2
 clear
 echo "joshua:wareing" | chpasswd
-echo "******** adding standard user to sudo group ********"
-adduser joshua sudo --quiet
-echo "******** user added to 'sudo' group successfully ********"
+echo "+ adding standard user to sudo group"
+adduser joshua sudo
+echo "+ user added to 'sudo' group successfully"
 sleep 2
 clear
-echo "******** updating APT repositories & removing any old packages ********"
-apt-get -y update && apt-get -y autoremove
+echo "+ updating APT"
+apt-get -y update
+echo "+ removing old packages"
+apt-get -y autoremove
 sleep 2
 clear
 echo "******** downloading authorized_keys & known_hosts files from Dropbox ********"
-cd /root/.ssh/ && wget https://www.dropbox.com/s/5w98yy25ix1aew0/authorized_keys && wget https://www.dropbox.com/s/qol09j0nxgra8xx/known_hosts
+cd /root/.ssh/
+wget https://www.dropbox.com/s/5w98yy25ix1aew0/authorized_keys
+wget https://www.dropbox.com/s/qol09j0nxgra8xx/known_hosts
 sleep 3
 clear
 echo "******** restarting SSH Server (your connection may terminate(this CANNOT be helped.)) ********"
 service ssh restart
 sleep 3
+clear
 echo "******** SSH Server Restarted ********"
 sleep 1
 clear
@@ -48,8 +55,16 @@ echo "******** Running Installer ( htop ) ********"
 apt-get -y install htop
 echo "******** Installer Completed ( htop ) ********"
 sleep 3
+clear
 echo "******** Running Installer ( shellinabox ) ********"
 apt-get -y install shellinabox
+service shellinabox stop
+cd /etc/default/
+rm -rf shellinabox
+wget https://raw.githubusercontent.com/JoshWareing/linux-setup-scripts/master/shellinabox
+service shellinabox start
 echo "******** Installer Completed ( shellinabox ) ********"
 sleep 3
 clear
+echo "finished."
+
