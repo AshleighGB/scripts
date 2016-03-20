9de94502b05e81f083e6851d7ffcf7e22360ea4f
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-03-20 23:52:07 +0100

    Create web-server.sh

---
+++ web-server.sh
@@ -0,0 +1,96 @@
+clear
+echo "/ changing root password"
+echo "root:R0sebr1dge" | chpasswd
+sleep 2
+clear
+echo "+ applying custom configs for remote logins"
+touch /etc/skel/.hushlogin
+rm -rf /etc/motd
+rm -rf /etc/issue
+rm -rf /etc/issue.net
+rm -rf /root/.ssh/known_hosts
+rm -rf /root/.ssh/authorized_keys
+sleep 2
+clear
+echo "- removing old sshd_config"
+cd /etc/ssh/ && rm sshd_config
+echo "+ adding custom sshd_config"
+wget https://www.dropbox.com/s/05f96jvwddl09qo/sshd_config
+sleep 2
+clear
+echo "+ adding standard user"
+adduser --disabled-password -shell /bin/bash --home /home/joshua --gecos "Joshua Wareing" joshua
+echo "+ user added successfully "
+sleep 2
+clear
+echo "joshua:wareing" | chpasswd
+echo "+ adding standard user to sudo group"
+adduser joshua sudo
+echo "+ user added to 'sudo' group successfully"
+sleep 2
+clear
+echo "+ adding standard user to sudo group"
+adduser joshua sudo
+echo "+ adding monitoring user"
+mkdir /home/monitor
+mkdir /home/monitor/.ssh/
+echo "monitor:monitor" | chpasswd
+adduser --disabled-password -shell /usr/bin/htop --home /home/monitor --gecos "System Monitor" monitor
+echo "[complete]"
+echo "+ adding user to sudo group"
+adduser monitor sudo
+echo "+ user added to 'sudo' group successfully"
+echo "+ updating APT"
+apt-get -y update
+echo "+ removing old packages"
+apt-get -y autoremove
+sleep 2
+clear
+echo "******** downloading authorized_keys & known_hosts files from Dropbox ********"
+cd /root/.ssh/
+wget https://www.dropbox.com/s/5w98yy25ix1aew0/authorized_keys
+wget https://www.dropbox.com/s/qol09j0nxgra8xx/known_hosts
+cd /tmp/ && wget https://raw.githubusercontent.com/JoshWareing/linux-setup-scripts/master/monitor_ssh_key.sh && chmod +x monitor_ssh_key.sh && ./monitor_ssh_key.sh
+sleep 3
+clear
+echo "******** restarting SSH Server (your connection may terminate(this CANNOT be helped.)) ********"
+service ssh restart
+sleep 3
+clear
+echo "******** SSH Server Restarted ********"
+sleep 1
+clear
+echo "******** ssh security has been successfully configured.  ********"
+sleep 3
+clear
+echo "******** Running Installer ( htop ) ********"
+apt-get -y install htop
+echo "******** Installer Completed ( htop ) ********"
+sleep 3
+clear
+echo "******** Running Installer ( shellinabox ) ********"
+apt-get -y install shellinabox
+service shellinabox stop
+cd /etc/default/
+rm -rf shellinabox
+wget https://raw.githubusercontent.com/JoshWareing/linux-setup-scripts/master/shellinabox
+service shellinabox start
+echo "******** Installer Completed ( shellinabox ) ********"
+sleep 3
+clear
+echo "******** Running Installer ( apache2 ) ********"
+apt-get -y install apache2
+service apache2 stop
+service apache2 start
+echo "******** Installer Completed ( apache2 ) ********"
+sleep 3
+clear
+echo "******** Running Installer ( php5 ) ********"
+apt-get -y install php5 php5-mysql php5-gd php5-imagick
+service apache2 stop
+service apache2 start
+echo "******** Installer Completed ( php5 ) ********"
+sleep 3
+clear
+
+echo "finished."
