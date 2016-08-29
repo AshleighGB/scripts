444e429c840589a27b7c750430cef84fb4e6d64d
Joshua Wareing <JoshWareing@users.noreply.github.com>
2016-08-29 15:30:03 +0100

    Create mono-40-install.sh

---
+++ mono-40-install.sh
@@ -0,0 +1,8 @@
+echo "Running action: Install private key for repo"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "Running action: Add Mono Repository"
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "Running action: Update Upgrade Packages"
+sudo apt-get -y update && apt-get -y upgrade
+echo "Running action: Install Mono"
+apt-get -y install mono-complete
