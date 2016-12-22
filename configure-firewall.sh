b7a026722d344c58bd5f33626aa3d3b846e3bdd4
Wareing <JoshWareing@users.noreply.github.com>
2016-12-22 09:38:41 +0000

    Update configure-firewall.sh

--- configure-firewall.sh
+++ configure-firewall.sh
@@ -1,6 +1,12 @@
 iptables -p INPUT DROP
+iptables -p 
 iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --dport 22 -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 25565 -j ACCEPT
 iptables -A INPUT -i eth0 -p udp --dport 25565 -j ACCEPT
-iptables --save
+iptables -A OUTPUT -i eth0 -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -i eth0 -p udp --dport 22 -j ACCEPT
+iptables -A OUTPUT -i eth0 -p tcp --dport 25565 -j ACCEPT
+iptables -A OUTPUT -i eth0 -p udp --dport 25565 -j ACCEPT
+iptables-save
+reboot
